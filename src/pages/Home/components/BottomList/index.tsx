05c2c475143f482c04846b251d725b3a145c720a
Secret Elf <iamsecretsanta124@protonmail.com>
2021-01-19 09:21:59 -0600

    add coin gecko link

--- src/pages/Home/components/BottomList/index.tsx
+++ src/pages/Home/components/BottomList/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const BottomList = ({ goToSite }) => (
-  <ul className="bottom-list disabled">
-    <li className="animated fadeInUpMicro d300 delay100">
+  <ul className="bottom-list">
+    <li className="animated fadeInUpMicro d300 delay100" onClick={() => goToSite('https://www.coingecko.com/en/coins/zero-exchange')}>
       <img src="/coingecko.png" alt="CoinGecko" className="white-bg" />
     </li>
-    <li className="animated fadeInUpMicro d300 delay150">
+    <li className="animated fadeInUpMicro d300 delay150 disabled">
       <img src="/coinmarketcap.png" alt="CoinMarketCap" className="white-bg" />
     </li>
-    <li className="animated fadeInUpMicro d300 delay200">
+    <li className="animated fadeInUpMicro d300 delay200 disabled">
       <img src="/defipulse.png" alt="DefiPulse" className="white-bg" />
     </li>
   </ul>
