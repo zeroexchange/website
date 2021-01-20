8739de6302b02bc49571de3b840ef8e3c529cc11
iamsecretsanta <74973154+iamsecretsanta@users.noreply.github.com>
2021-01-20 09:13:59 +0100

    Update index.tsx

--- src/pages/Home/components/BottomList/index.tsx
+++ src/pages/Home/components/BottomList/index.tsx
@@ -5,7 +5,7 @@ const BottomList = ({ goToSite }) => (
     <li className="animated fadeInUpMicro d300 delay100" onClick={() => goToSite('https://www.coingecko.com/en/coins/zero-exchange')}>
       <img src="/coingecko.png" alt="CoinGecko" className="white-bg" />
     </li>
-    <li className="animated fadeInUpMicro d300 delay150 disabled">
+    <li className="animated fadeInUpMicro d300 delay150" onClick={() => goToSite('https://coinmarketcap.com/currencies/zero-exchange/')}>
       <img src="/coinmarketcap.png" alt="CoinMarketCap" className="white-bg" />
     </li>
     <li className="animated fadeInUpMicro d300 delay200 disabled">
